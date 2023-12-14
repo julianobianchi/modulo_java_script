4ef142b73fd7db2277bb54ebc3e2c17dd3faaad3
Juliano Bianchi <julianobianchi@gmail.com>
2023-12-13 22:17:44 -0300

    aula06

--- aula_06/assets/js/requisicoes.js
+++ aula_06/assets/js/requisicoes.js
@@ -45,15 +45,31 @@ const renderPokemons = function () {
     const btn_next = document.getElementById('next');
 
     dados.results.forEach(item => {
-        div_items.innerHTML += `<h1>${item.name}</h1>`;
+        div_items.innerHTML += `<h1 style="padding:5px">${item.name}</h1>`;
     });
 
     if (dados.previous == null) {
-        btn_previous.disabled = true
+        btn_previous.disabled = true  
+    }
+    else{
+        if(btn_previous.disabled == true){
+            btn_previous.disabled =false;
+        }        
+        btn_previous.onclick =  function(){
+            consultaEndpoint('pokemon', dados.previous);
+        }        
     }
 
     if (dados.next == null) {
         btn_next.disabled = true
+    }else{
+        if(btn_next.disabled == true){
+            btn_next.disabled =false;
+        }        
+        btn_next.onclick =  function(){
+            consultaEndpoint('pokemon', dados.next);
+        }        
+        
     }
 }
 
